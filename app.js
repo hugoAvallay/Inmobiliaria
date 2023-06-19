5b4968878b7764a6f28dfc167c6ecc62c8ddfd2a
hugoAvallay <h40hugo@gmail.com>
2023-06-18 23:18:00 -0300

    add file and finished html structure

--- app.js
+++ app.js
@@ -3,5 +3,18 @@ const path = require('path');
 const app = express();
 const PORT = 4530;
 
-app.get('/',(re,res)=>res.sendFile(path.join(__dirname,'views/index.html')))
+app.use(express.static('public'));
+
+app.get('/',(req,res)=>res.sendFile(path.join(__dirname,'views/home.html')));
+
+app.get('/Anuncios',(req,res)=>res.sendFile(path.join(__dirname,'views/Anuncios.html')));
+
+app.get('/Nosotros',(req,res)=>res.sendFile(path.join(__dirname,'/views/Nosotros.html')));
+
+app.get('/Blog',(req,res)=>res.sendFile(path.join(__dirname,'/views/Blog.html')));
+
+app.get('/Contacto',(req,res)=>res.sendFile(path.join(__dirname,'views/Contacto.html')))
+
+
+
 app.listen(PORT,()=>console.log(`Server running in http://localhost:${PORT}`));
